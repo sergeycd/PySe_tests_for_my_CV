28e1d49630f2caa1eb3d2d7201fbcbee902700a2
Golovanov Sergey <golovanov911@gmail.com>
2023-09-04 19:16:09 +0700

    Добавил тест на проверку корреткности работы кнопки "Like!"

--- AT_CV/tests/tests.py
+++ AT_CV/tests/tests.py
@@ -13,7 +13,6 @@ class TestElements:
             str_url = str(get_url)
             print(str_url)
             assert str_url == "https://sergeycd.github.io/New_CV/index.html"
-            time.sleep(3)
 
         def test_name_author(self, driver):
             page = TestsOfPage(driver, 'https://sergeycd.github.io/New_CV/index.html')
@@ -21,3 +20,15 @@ class TestElements:
             page.test_name_of_author()
             get_name = page.test_name_of_author()
             assert get_name == "Голованов Сергей"
+
+        def test_open_tg(self, driver):
+            page = TestsOfPage(driver, 'https://sergeycd.github.io/New_CV/index.html')
+            page.open()
+            page.test_open_tm()
+            swith_to_alert = driver.switch_to.alert
+            swith_to_alert.accept()
+            time.sleep(3)
+            driver.switch_to.window(driver.window_handles[1])
+            get_url_tm = driver.current_url
+            str_url = str(get_url_tm)
+            assert str_url == "https://t.me/cdpctr"
